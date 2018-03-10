93b1ab18c13b6fa1ec6178660d77b348d52d5c36
DESKTOP-HSUOM0P\WickedCube <wickedcube@gmail.com>
2018-03-10 14:18:19 +0530

    Init commit
    Init commit

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public static EnemySpawner instance;
+
+	public Transform[] enemy_spawn_pos_arr;
+	public int[] enemy_spawn_per_second_per_level;
+	public Coroutine enemy_spawn_coroutine;
+	public Queue<Transform> enemy_queue;
+	public Transform[] enemy_prefabs_arr;
+
+	void Awake() {
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		enemy_spawn_coroutine = StartCoroutine(EnemySpawnCoroutine());
+	}
+
+	IEnumerator EnemySpawnCoroutine() {
+		yield return new WaitForSeconds(enemy_spawn_per_second_per_level[GameManager.instance.current_level]);
+		Transform enemy_instance = Instantiate(enemy_prefabs_arr[Random.Range(0,enemy_prefabs_arr.Length)],enemy_spawn_pos_arr[Random.Range(0,enemy_spawn_pos_arr.Length)]);
+		enemy_queue.Enqueue(enemy_instance);
+		enemy_spawn_coroutine = StartCoroutine(EnemySpawnCoroutine());
+	}
+
+
+	public Transform OldestEnemyAlive() {
+		return enemy_queue.Dequeue();
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	[HideInInspector]
+	public int current_level = 0;
+	[HideInInspector]
+	public int[] time_per_level;
+	Coroutine level_update_coroutine;
+	void Awake() {
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		level_update_coroutine = StartCoroutine(UpdateLevelTime());
+	}
+
+	IEnumerator UpdateLevelTime() {
+		yield return new WaitForSeconds(time_per_level[current_level]);
+		current_level++;
+		level_update_coroutine = StartCoroutine(UpdateLevelTime());
+	}
+}
